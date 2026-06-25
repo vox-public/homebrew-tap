15bcfb39eb8022e4b507c6d02715b5be67789b7c
kernelgarden <kernelgarden@gmail.com>
2026-06-25 16:57:12 +0900

    Bottle vox 0.1.0-beta.2

--- Formula/vox.rb
+++ Formula/vox.rb
@@ -2,7 +2,6 @@ class Vox < Formula
   desc "Command-line workflow tools for vox.ai"
   homepage "https://www.tryvox.co"
   license "MIT"
-  depends_on "sox"
 
   on_macos do
     if Hardware::CPU.arm?
@@ -12,6 +11,16 @@ class Vox < Formula
       url "https://github.com/vox-public/homebrew-tap/releases/download/v0.1.0-beta.2/vox-v0.1.0-beta.2-darwin-x64.tar.gz"
       sha256 "c2948a78a58b7433683024772c2a0b0935e407107aca234259b8f79e75db43af"
     end
+  end
+
+  bottle do
+    root_url "https://github.com/vox-public/homebrew-tap/releases/download/v0.1.0-beta.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ffcac5a685226f6c0af14fe407ad94005a9fccf7997b63045ae0d703704f27df"
+    sha256 cellar: :any_skip_relocation, tahoe:         "12e5717650986730d71ac8a7d2fcac2aa8576fdba72e52565cbd9333aad4c141"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "09e265e21af8775778d0a929685713ad6ddbdeacf41f5c3401c90d839f41aa39"
+    sha256 cellar: :any_skip_relocation, sequoia:       "75795b8f917c8d67e9f388dc8fb2151d4039e5820705c7e4ad46d36760a1f803"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "47147d1a17ff931982097521e5c84c0666981e150568358ff80ee12eb83282cf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "570f85dfce7313903a755d22cfba23df6df50945710bc6552530ee51c6c092e0"
   end
 
   def install
